0cb01489f375928c81682ef9cdbdfdfe78757f2c
naikoob <bookian@gmail.com>
2020-10-22 22:29:41 +0800

    add logging

--- greeting-rs/src/main/java/eap/sample/greeting/GreetingService.java
+++ greeting-rs/src/main/java/eap/sample/greeting/GreetingService.java
@@ -1,8 +1,14 @@
 package eap.sample.greeting;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GreetingService {
 
+	private static final Logger LOG = LoggerFactory.getLogger(GreetingService.class);
+
 	String greet(String name) {
+		LOG.info("greetings, {}", name);
 		return "Hello " + name + "!";
 	}
 }
